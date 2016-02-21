4617ba88298feed00c4a03e52c8626e0372e5411
Gustavo Rodrigues <qgustavor-git-commits@blio.tk>
2016-02-21 15:51:56 -0300

    Fix stats
    Split watched into "finished" and "non-finished".
    Consider when multiple episodes are watched in a row.

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,6 +6,7 @@
     nome: 'Nome',
     data: 'Data',
     episodio: 'Episódio',
+    episodios: 'Episódios',
     duracao: 'Duração'
   };
   
@@ -17,7 +18,23 @@
   function onLoaded (sheets) {
     document.getElementById('loading-message').style.display = 'none';
     
-    var assistido = sheets[keys.assistido].elements;
+    var assistido = sheets[keys.assistido].elements.sort(function (a, b) {
+      return a[keys.data] === b[keys.data] ? 0 : +(a[keys.data] > b[keys.data]) || -1;
+    });
+    
+    var animes = sheets[keys.animes].elements.reduce(function (acc, element) {
+      acc[element[keys.nome]] = element;
+      return acc;
+    }, {});
+    
+    var finishedAnime = Object.keys(assistido.reduce(function (finished, e) {
+      var data = animes[e[keys.nome]];
+      if (data && +data[keys.episodios] === +e[keys.episodio]) {
+        finished[e[keys.nome]] = true;
+      }
+      return finished;
+    }, {}));
+    
     var names = assistido.map(function (e) {
       return e[keys.nome];
     });
@@ -26,30 +43,41 @@
       return i === names.indexOf(e);
     });
     
-    var animes = sheets[keys.animes].elements.reduce(function (acc, element) {
-      acc[element[keys.nome]] = element;
-      return acc;
-    }, {});
-    
+    var lastEpisodeData = {};
+    var watchedEpisodeCount = 0;
     var days = assistido.reduce(function (acc, element) {
-      return acc += animes[element[keys.nome]][keys.duracao] | 0;
+      var animeName = element[keys.nome];
+      var animeData = animes[animeName];
+      var duration = animeData ? animeData[keys.duracao] : 24;
+      var watchedEpisodes = element[keys.episodio] - (lastEpisodeData[animeName] || 0);
+      var watchedTime = watchedEpisodes * duration;
+      watchedEpisodeCount += watchedEpisodes;
+      lastEpisodeData[animeName] = element[keys.episodio];
+      return acc += watchedTime;
     }, 0) / 60 / 24;
     
+    var watchingCount = uniqueNames.length - finishedAnime.length;
+    
     var gaugeStats = [{
-      title: 'Watched animes',
+      title: 'Finished anime',
       label: 'anime',
-      value: uniqueNames.length,
-      max: Math.floor(uniqueNames.length / 100 + 1) * 100
+      value: finishedAnime.length,
+      max: Math.floor(finishedAnime.length / 100 + 1) * 100
     }, {
       title: 'Watched episodes',
       label: 'episodes',
-      value: assistido.length,
-      max: Math.floor(assistido.length / 1000 + 1) * 1000
+      value: watchedEpisodeCount,
+      max: Math.floor(watchedEpisodeCount / 1000 + 1) * 1000
     }, {
       title: 'Watched days',
       label: 'days',
       value: days,
       max: Math.floor(days / 30 + 1) * 30
+    }, {
+      title: 'Non-finished anime',
+      label: 'anime',
+      value: watchingCount,
+      max: Math.floor(watchingCount / 10 + 1) * 10
     }];
     
     gaugeStats.forEach(renderGaugeStat);
@@ -62,7 +90,7 @@
     
   function renderGaugeStat(stat) {
     element = document.createElement('div');
-    element.className = 'col-sm-' + (stat.size || 4);
+    element.className = 'col-sm-' + (stat.size || 3);
     element.innerHTML = '<div class="chart-wrapper"><div class="chart-title">' + stat.title + '</div><div class="chart-stage">' +
     '<div class="chart-holder"></div></div></div>';
     container.appendChild(element);
